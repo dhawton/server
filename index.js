3424d22994b47ec4a82e36e74e69902ce8408d43
Daniel Hawton <daniel@hawton.org>
2019-07-17 21:15:38 -0800

    add rcon and auto-restart

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
 const config = require("./config.json");
 const { spawn } = require("child_process");
 const fs = require("fs");
+const rcon = require("rcon");
 
 let fx = null;
 let fd = null;
-let isStarting = false;
+let firstStart = true;
+let crashed = false;
+let dailyRestart = 0;
+let conn = null;
 
 console.log("MASTER CONTROL PROGRAM by Daniel A. Hawton");
 console.log(`Starting control processing for ${config.server}`);
+console.log("");
 
 const mainLoop = () => {
-  if (fx === null && !isStarting) {
-    console.log("Server does not appear to be running, attempting to start");
+  if (firstStart) {
+    console.log(`Starting up server.`);
     startFx();
-  }
-
-  if (fs.existsSync(`${config.restartFlag}`)) {
+    firstStart = false;
+  } else if (crashed) {
+    console.log("Server appears to have crashed... restarting...");
+    startFx();
+  } else if (fs.existsSync(`${config.restartFlag}`)) {
     console.log("Restart flag found, restarting server.");
-    fs.unlink(`${config.restartFlag}`);
-    isStarting = true;
+    fs.unlinkSync(`${config.restartFlag}`);
     startFx();
+  } else {
+    const d = new Date();
+    if (d.getHours() === 4 && d.getMinutes() > 56 && dailyRestart < 4) {
+      console.log(`Starting checks for automatic restart at 0500`);
+      if (!conn)
+        conn = new rcon(config.hostname, config.port, config.password, {
+          tcp: false,
+          challenge: false
+        });
+      if (d.getMinutes() === 57 && dailyRestart === 0) {
+        console.log(`3 minutes to shutdown.`);
+        conn.send(
+          "shutdown Reports of an tsunami roaring toward the island that will end the world in ~r~3~w~ minutes! Evacuate now!"
+        );
+        dailyRestart = 1;
+      }
+      if (d.getMinutes() === 58 && dailyRestart === 1) {
+        console.log(`2 minutes to shutdown.`);
+        conn.send(
+          "shutdown The world will end in ~r~2~w~ minutes. Evacuate now!"
+        );
+        dailyRestart = 2;
+      }
+      if (d.getMinutes() === 59 && dailyRestart === 2) {
+        console.log(`1 minute to shutdown.`);
+        conn.send(
+          "shutdown The world will end in ~r~1~w~ minute. Evacuate now!"
+        );
+        dailyRestart = 3;
+      }
+    } else if (
+      d.getHours() === 5 &&
+      d.getMinutes() === 0 &&
+      dailyRestart !== 4
+    ) {
+      console.log(`Commencing restart...`);
+      dailyRestart = 4;
+      startFx();
+      dailyRestart = 0;
+    }
   }
 };
 
@@ -29,11 +75,13 @@ const resetLogs = () => {
     let fn =
       d.getFullYear() +
       "-" +
-      (d.getMonth() > 10 ? "0" : "") +
+      (d.getMonth() < 10 ? "0" : "") +
       d.getMonth() +
       "-" +
-      (d.getDate() > 10 ? "0" : "") +
-      d.getDate();
+      (d.getDate() < 10 ? "0" : "") +
+      " " +
+      d.getHours() +
+      d.getMinutes();
     fs.renameSync(
       `${config.logPath}\\${config.logPrefix}.log`,
       `${config.logPath}\\${config.logPrefix}-${fn}.log`
@@ -53,18 +101,13 @@ const startFx = () => {
   fd = fs.createWriteStream(`${config.logPath}\\${config.logPrefix}.log`, {
     flags: "w"
   });
-  isStarting = true;
   fx = spawn(config.cmd);
   fx.stdout.pipe(fd);
   fx.stderr.pipe(fd);
-  fx.on("close", () => {
-    fx = null;
+  fx.on("exit", () => {
+    crashed = true;
   });
-  fx.stdout.on("data", data => {
-    fs.write(fd, data);
-  });
-  isStarting = false;
 };
 
 mainLoop();
-setInterval(mainLoop, 30000);
+setInterval(mainLoop, 10000);
